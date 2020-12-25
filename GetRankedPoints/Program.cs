e372efa9b18009351af78a6eedea30734f9680f7
Paulo khayat <paulokhayat@gmail.com>
2020-12-25 12:54:40 +0200

    Add credential input if no config file is detected

--- GetRankedPoints/Program.cs
+++ GetRankedPoints/Program.cs
@@ -24,16 +24,37 @@ namespace GetRankedPoints
             Console.WriteLine("Checking Config File.."); 
             if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "config.json")))
             {
-                Console.WriteLine("Config File not found, please add config file.");
-                Console.ReadKey();
-                Environment.Exit(1);
+                Console.WriteLine("Config File not found, enter your username:");
+                username = Console.ReadLine();
+                Console.WriteLine("enter password");
+                var pass = string.Empty;
+                ConsoleKey key;
+                do
+                {
+                    var keyInfo = Console.ReadKey(intercept: true);
+                    key = keyInfo.Key;
+
+                    if (key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        Console.Write("\b \b");
+                        pass = pass[0..^1];
+                    }
+                    else if (!char.IsControl(keyInfo.KeyChar))
+                    {
+                        Console.Write("*");
+                        pass += keyInfo.KeyChar;
+                    }
+                } while (key != ConsoleKey.Enter);
+                password=pass;
+                Console.WriteLine("\nenter region (eu/na/ap/ko/br): ");
+                region=Console.ReadLine();
             }
-            else
+            else{
                 Console.WriteLine("Config File found, Reading File...");
 
-            ReadConfig();
-            Console.WriteLine("Finished Reading Config, Logging in..");
-            
+                ReadConfig();
+                Console.WriteLine("Finished Reading Config, Logging in..");
+            }
             Login();
             Console.WriteLine("Finished Logging in, checking rank progression...");
             CheckRankedUpdates();
@@ -80,7 +101,7 @@ namespace GetRankedPoints
 
                 EntitlementToken = entitlement_tokenObj["entitlements_token"].Value<string>();
 
-                
+
                 RestClient userid_client = new RestClient(new Uri("https://auth.riotgames.com/userinfo"));
                 RestRequest userid_request = new RestRequest(Method.POST);
 
